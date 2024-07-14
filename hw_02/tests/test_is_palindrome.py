5feafb9f1cbcdadd4dd7901f5b6ebf0f0a7a8855
nanashi <egorglazkov71@gmail.com>
2024-07-14 18:12:05 +0300

    add test for is polindrome

---
+++ hw_02/tests/test_is_palindrome.py
@@ -0,0 +1,24 @@
+from hw_02.src.main_2 import is_palindrome
+import pytest
+
+
+@pytest.mark.parametrize("value, expected_result",
+                         [
+                             (512, False),
+                             (121, True),
+                             (-121, False),
+                             (190, False),
+                             (0, True),
+                         ])
+def test_is_palindrome_positive(value, expected_result):
+    assert is_palindrome(value) == expected_result
+
+
+@pytest.mark.parametrize("value, expected_result",
+                         [
+                             ("str", pytest.raises(TypeError)),
+                             (1.2, pytest.raises(TypeError)),
+                         ])
+def test_is_palindrome_negative(value, expected_result):
+    with expected_result:
+        assert is_palindrome(value) == expected_result
